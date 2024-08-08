7c773f4de68c0cf9d8c8ce9e54fe9dec226ddefe
Your Name <you@example.com>
2024-08-08 21:37:29 +0500

    Mouse

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGithub, FaFacebook, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin ,FaLink } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -11,15 +11,13 @@ const Footer = () => {
             <h2 className='font-bold hover:text-active duration-300 py-1 hover:cursor-pointer'>Specification</h2>
         </div>
         <div className='flex  items-center justify-center gap-5 mx-auto'>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/developer-khadim" target="_blank" rel="noopener noreferrer">
                 <FaGithub className='text-white hover:text-active' size={25} />
             </a>
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                <FaFacebook className='text-white hover:text-active' size={25} />
-            </a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/khadim-ali12/" target="_blank" rel="noopener noreferrer">
                 <FaLinkedin className='text-white hover:text-active' size={25} />
             </a>
+            <a href="https://khadim-portfolio.vercel.app/" target='_blank' > <FaLink  className='hover:text-active' size={25} /> </a>
         </div>
     </footer>
   )
